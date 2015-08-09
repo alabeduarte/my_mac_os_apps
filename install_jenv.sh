f7cd0c99b767760c0499b908a4c3948d454da43d
Alabê Duarte <alabeduarte@gmail.com>
2015-08-09 16:03:19 -0300

    Add feedback message before install

--- install_jenv.sh
+++ install_jenv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo 'Prepare to install jenv'
+
 brew install jenv
 
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
--- install_nvm.sh
+++ install_nvm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo 'Prepare to install nvm'
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh |
 bash
