88fbbe6cd7af272b317381c7157335739efa2245
Alabê Duarte <alabeduarte@gmail.com>
2015-08-26 12:58:55 -0300

    Set var envs and aliases

--- install_development_tools.sh
+++ install_development_tools.sh
@@ -3,11 +3,17 @@
 echo 'Prepare to install development tools'
 
 brew install rbenv
+
 brew install tmux
+echo 'export TERM=xterm-256color' >> ~/.zshrc
 
 brew cask install iterm2
+
 brew cask install virtualbox
+
 brew cask install vagrant
+echo 'alias vg="vagrant"' >> ~/.zshrc
+echo 'alias v="vim"' >> ~/.zshrc
 
 sh ./development-tools/install_tmuxinator.sh
 sh ./development-tools/install_jenv.sh
