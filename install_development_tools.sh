c6d1141ccfab7210ac4f3cfb5b70145ebf18bd99
Alabê Duarte <alabeduarte@gmail.com>
2015-08-28 10:23:39 -0300

    Add dockertoolbox

--- install_development_tools.sh
+++ install_development_tools.sh
@@ -7,15 +7,20 @@ brew install rbenv
 brew install tmux
 echo 'export TERM=xterm-256color' >> ~/.zshrc
 
+sh ./development-tools/install_tmuxinator.sh
+
 brew cask install iterm2
 
 brew cask install virtualbox
 
+# requires root privileges
+sudo brew cask install dockertoolbox
+
 brew cask install vagrant
 echo 'alias vg="vagrant"' >> ~/.zshrc
-echo 'alias v="vim"' >> ~/.zshrc
 
-sh ./development-tools/install_tmuxinator.sh
 sh ./development-tools/install_jenv.sh
 sh ./development-tools/install_nvm.sh
+
 sh ./development-tools/install_vim_files.sh
+echo 'alias v="vim"' >> ~/.zshrc
