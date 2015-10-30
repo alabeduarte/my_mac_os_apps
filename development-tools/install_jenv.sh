f50c6b6614cb6a14916b42a1950d4c1740328a69
Alabê Duarte <alabeduarte@gmail.com>
2015-10-29 23:50:53 -0300

    Fix jenv init

--- development-tools/install_jenv.sh
+++ development-tools/install_jenv.sh
@@ -5,4 +5,4 @@ echo 'Prepare to install jenv'
 brew install jenv
 
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(jenv init -)"' >> ~/.zshrc
+echo 'if which jenv > /dev/null; then eval "$(jenv init - zsh)"; fi' >> ~./zshrc
