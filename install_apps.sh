a5bbec611a5db6485826bfd489cf4a41bcb9de36
Alabe Duarte <alabeduarte@gmail.com>
2017-12-01 21:47:26 +1100

    fix(google-backup-and-sync): replace former google drive by new one

--- install_apps.sh
+++ install_apps.sh
@@ -10,7 +10,7 @@ brew cask install spectacle
 brew cask install dropbox
 brew cask install 1password
 brew cask install google-chrome
-brew cask install google-drive
+brew cask install google-backup-and-sync
 brew cask install slack
 brew cask install vlc
 brew install htop
